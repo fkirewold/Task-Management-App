8f306c602b816f50ec35883f1e1429ed7160d3d9
fkir21 <addimanzerihun@gmail.com>
2025-02-26 13:06:36 +0300

    bug fixed

--- lib/repositories/blog_repository.dart
+++ lib/repositories/blog_repository.dart
@@ -6,19 +6,23 @@ import 'package:todo/models/blog_model.dart';
 
 class BlogRepository {
   Future<List<BlogModel>> fetchPosts() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
+    try {
+      var connectivityResult = await Connectivity().checkConnectivity();
 
-    if (connectivityResult.contains(ConnectivityResult.none)) {
-      throw 'No Internet';
-    }
-    final response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body) as List;
-      final blogs = data.map((d) => BlogModel.fromMap(d)).toList();
-      return blogs;
-    } else {
-      throw 'Failed to load posts';
+      if (connectivityResult.contains(ConnectivityResult.none)) {
+        throw 'No Internet';
+      }
+      final response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List;
+        final blogs = data.map((d) => BlogModel.fromMap(d)).toList();
+        return blogs;
+      } else {
+        throw 'Failed to load posts';
+      }
+    } catch (e) {
+      throw 'Something gone Wrong';
     }
   }
 }
